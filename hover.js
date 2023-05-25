2b9a4d51e45c1be09ae0ccca7b72c5c973fcd4f2
nurgazievbekbolsun <nurgazievbekbolsun906@gmail.com>
2023-05-25 15:46:18 +0600

    piano

---
+++ hover.js
@@ -0,0 +1,196 @@
+const block = document.querySelector('#board')
+const audio = document.querySelector('.audio')
+const  btn = document.querySelector('#myBtn')
+
+const colorNames = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+];
+for (let a = 0;a < 304;a++){
+    const beka = document.createElement('div')
+    block.append(beka)
+    beka.setAttribute('class','box')
+    beka.addEventListener('mouseover',()=>{
+        setColor(beka)
+        myFunction(audio.pause())
+    })
+    beka.addEventListener('mouseout',()=>{
+        removeColor(beka)
+        myFunction(audio.play())
+
+    })
+}
+function setColor(a){
+    let color = randomColor
+    a.style.background = randomColor()
+        a.style.boxShadow = `2px 2px 3px ${color},0 0 10px ${color}`
+    a.style.transform = 'scale(1.2)'
+}
+function removeColor(b){
+    b.style.background = '#777474';
+    b.style.transform = 'scale(1)'
+    b.style.boxShadow = '0 0 2px #000'
+}
+function randomColor(){
+const idx = Math.round(Math.random() * colorNames.length)
+return colorNames[idx]
+}
+
+
+function myFunction() {
+    if (audio.paused) {
+        audio.play();
+        btn.style.display = "none";
+        // btn.innerHTML = "Pause";
+    } else {
+        audio.pause();
+        // btn.innerHTML = "Play";
+        btn.style.display = "none";
+    }
+}
---
+++ randomBlocBg.js
@@ -0,0 +1,781 @@
+const bloc = document.querySelectorAll('.block')
+const audio = document.querySelector('.audi')
+const audioo = document.querySelector('.audir')
+const btn = document.querySelector('.bas')
+
+const play = document.querySelector('.bla21')
+const play1 = document.querySelector('.bla20')
+const play2 = document.querySelector('.bla19')
+const play3 = document.querySelector('.bla18')
+const play4 = document.querySelector('.bla17')
+const play5 = document.querySelector('.bla16 ')
+const play6 = document.querySelector('.bla15')
+const play7 = document.querySelector('.bla14')
+const play8 = document.querySelector('.bla13')
+const play9 = document.querySelector('.bla12')
+const play10 = document.querySelector('.bla11')
+const play11 = document.querySelector('.bla10')
+const play12 = document.querySelector('.bla9')
+const play13 = document.querySelector('.bla8')
+const play14 = document.querySelector('.bla7')
+
+play.addEventListener('click', () => {
+    kf1(k1.pause())
+    play.style.transform = 'scale(.9)'
+})
+play.addEventListener('mouseout', () => {
+    kf1(k1.play())
+    play.style.transform = 'scale(1)'
+})
+play1.addEventListener('click', () => {
+    kf2(k2.pause())
+    play1.style.transform = 'scale(.9)'
+})
+play1.addEventListener('mouseout', () => {
+    kf2(k2.play())
+    play1.style.transform = 'scale(1)'
+})
+
+play2.addEventListener('click', () => {
+    kf3(k3.pause())
+    play2.style.transform = 'scale(.9)'
+})
+play2.addEventListener('mouseout', () => {
+    kf3(k3.play())
+    play2.style.transform = 'scale(1)'
+})
+
+play3.addEventListener('click', () => {
+    kf4(k4.pause())
+    play3.style.transform = 'scale(.9)'
+})
+play3.addEventListener('mouseout', () => {
+    kf4(k4.play())
+    play3.style.transform = 'scale(1)'
+})
+
+play4.addEventListener('click', () => {
+    kf5(k5.pause())
+    play4.style.transform = 'scale(.9)'
+})
+play4.addEventListener('mouseout', () => {
+    kf5(k5.play())
+    play4.style.transform = 'scale(1)'
+})
+
+play5.addEventListener('click', () => {
+    kf6(k6.pause())
+    play5.style.transform = 'scale(.9)'
+})
+play5.addEventListener('mouseout', () => {
+    kf6(k6.play())
+    play5.style.transform = 'scale(1)'
+})
+
+play6.addEventListener('click', () => {
+    kf7(k7.pause())
+    play6.style.transform = 'scale(.9)'
+})
+play6.addEventListener('mouseout', () => {
+    kf7(k7.play())
+    play6.style.transform = 'scale(1)'
+})
+
+play7.addEventListener('click', () => {
+    kf8(k8.pause())
+    play7.style.transform = 'scale(.9)'
+})
+play7.addEventListener('mouseout', () => {
+    kf8(k8.play())
+    play7.style.transform = 'scale(1)'
+})
+play8.addEventListener('click', () => {
+    kf9(k9.pause())
+    play8.style.transform = 'scale(.9)'
+})
+play8.addEventListener('mouseout', () => {
+    kf9(k9.play())
+    play8.style.transform = 'scale(1)'
+})
+
+play9.addEventListener('click', () => {
+    kf10(k10.pause())
+    play9.style.transform = 'scale(.9)'
+})
+play9.addEventListener('mouseout', () => {
+    kf10(k10.play())
+    play9.style.transform = 'scale(1)'
+})
+
+play10.addEventListener('click', () => {
+    kf11(k11.pause())
+    play10.style.transform = 'scale(.9)'
+})
+play10.addEventListener('mouseout', () => {
+    kf11(k11.play())
+    play10.style.transform = 'scale(1)'
+})
+
+play11.addEventListener('click', () => {
+    kf12(k12.pause())
+    play11.style.transform = 'scale(.9)'
+})
+play11.addEventListener('mouseout', () => {
+    kf12(k12.play())
+    play11.style.transform = 'scale(1)'
+})
+
+play12.addEventListener('click', () => {
+    kf13(k13.pause())
+    play12.style.transform = 'scale(.9)'
+})
+play12.addEventListener('mouseout', () => {
+    kf13(k13.play())
+    play12.style.transform = 'scale(1)'
+})
+play13.addEventListener('click', () => {
+    kf14(k14.pause())
+    play13.style.transform = 'scale(.9)'
+})
+play13.addEventListener('mouseout', () => {
+    kf14(k14.play())
+    play13.style.transform = 'scale(1)'
+})
+play14.addEventListener('click', () => {
+    kf15(k15.pause())
+    play14.style.transform = 'scale(.9)'
+})
+play14.addEventListener('mouseout', () => {
+    kf15(k15.play())
+    play14.style.transform = 'scale(1)'
+})
+
+const k1 = document.querySelector('.kk1')
+
+function kf1() {
+    if (k1.paused) {
+        k1.play();
+    } else {
+        k1.pause();
+    }
+}
+
+const k2 = document.querySelector('.kk2')
+
+function kf2() {
+    if (k2.paused) {
+        k2.play();
+    } else {
+        k2.pause();
+    }
+}
+
+const k3 = document.querySelector('.kk3')
+
+function kf3() {
+    if (k3.paused) {
+        k3.play();
+    } else {
+        k3.pause();
+    }
+}
+
+const k4 = document.querySelector('.kk4')
+
+function kf4() {
+    if (k4.paused) {
+        k4.play();
+    } else {
+        k4.pause();
+    }
+}
+
+const k5 = document.querySelector('.kk5')
+
+function kf5() {
+    if (k5.paused) {
+        k5.play();
+    } else {
+        k5.pause();
+    }
+}
+
+const k6 = document.querySelector('.kk6')
+
+function kf6() {
+    if (k6.paused) {
+        k6.play();
+    } else {
+        k6.pause();
+    }
+}
+
+const k7 = document.querySelector('.kk7')
+
+function kf7() {
+    if (k7.paused) {
+        k7.play();
+    } else {
+        k7.pause();
+    }
+}
+
+const k8 = document.querySelector('.kk8')
+
+function kf8() {
+    if (k8.paused) {
+        k8.play();
+    } else {
+        k8.pause();
+    }
+}
+
+const k9 = document.querySelector('.kk9')
+
+function kf9() {
+    if (k9.paused) {
+        k9.play();
+    } else {
+        k9.pause();
+    }
+}
+
+const k10 = document.querySelector('.kk10')
+
+function kf10() {
+    if (k10.paused) {
+        k10.play();
+    } else {
+        k10.pause();
+    }
+}
+
+const k11 = document.querySelector('.kk11')
+
+function kf11() {
+    if (k11.paused) {
+        k11.play();
+    } else {
+        k11.pause();
+    }
+}
+
+const k12 = document.querySelector('.kk12')
+
+function kf12() {
+    if (k12.paused) {
+        k12.play();
+    } else {
+        k12.pause();
+    }
+}
+
+const k13 = document.querySelector('.kk13')
+
+function kf13() {
+    if (k13.paused) {
+        k13.play();
+    } else {
+        k13.pause();
+    }
+}
+
+const k14 = document.querySelector('.kk14')
+
+function kf14() {
+    if (k14.paused) {
+        k14.play();
+    } else {
+        k14.pause();
+    }
+}
+
+const k15 = document.querySelector('.kk15')
+
+function kf15() {
+    if (k15.paused) {
+        k15.play();
+    } else {
+        k15.pause();
+    }
+}
+
+
+//
+const do3 = document.querySelector('.do3')
+
+function playdo3() {
+    if (do3.paused) {
+        do3.play();
+    } else {
+        do3.pause();
+    }
+}
+
+const do4 = document.querySelector('.do4')
+
+function playdo4() {
+    if (do4.paused) {
+        do4.play();
+    } else {
+        do4.pause();
+    }
+}
+
+const do5 = document.querySelector('.do5')
+
+function playdo5() {
+    if (do5.paused) {
+        do5.play();
+    } else {
+        do5.pause();
+    }
+}
+
+const re3 = document.querySelector('.re3')
+
+function playre3() {
+    if (re3.paused) {
+        re3.play();
+    } else {
+        re3.pause();
+    }
+}
+
+const re4 = document.querySelector('.re4')
+
+function playre4() {
+    if (re4.paused) {
+        re4.play();
+    } else {
+        re4.pause();
+    }
+}
+
+const re5 = document.querySelector('.re5')
+
+function playre5() {
+    if (re5.paused) {
+        re5.play();
+    } else {
+        re5.pause();
+    }
+}
+
+const mi3 = document.querySelector('.mi3')
+
+function playmi3() {
+    if (mi3.paused) {
+        mi3.play();
+    } else {
+        mi3.pause();
+    }
+}
+
+const mi4 = document.querySelector('.mi4')
+
+function playmi4() {
+    if (mi4.paused) {
+        mi4.play();
+    } else {
+        mi4.pause();
+    }
+}
+
+const mi5 = document.querySelector('.mi5')
+
+function playmi5() {
+    if (mi5.paused) {
+        mi5.play();
+    } else {
+        mi5.pause();
+    }
+}
+
+const fa3 = document.querySelector('.fa3')
+
+function playfa3() {
+    if (fa3.paused) {
+        fa3.play();
+    } else {
+        fa3.pause();
+    }
+}
+
+const fa4 = document.querySelector('.fa4')
+
+function playfa4() {
+    if (fa4.paused) {
+        fa4.play();
+    } else {
+        fa4.pause();
+    }
+}
+
+const fa5 = document.querySelector('.fa5')
+
+function playfa5() {
+    if (fa5.paused) {
+        fa5.play();
+    } else {
+        fa5.pause();
+    }
+}
+
+const sol3 = document.querySelector('.sol3')
+
+function playsol3() {
+    if (sol3.paused) {
+        sol3.play();
+    } else {
+        sol3.pause();
+    }
+}
+
+const sol4 = document.querySelector('.sol4')
+
+function playsol4() {
+    if (sol4.paused) {
+        sol4.play();
+    } else {
+        sol4.pause();
+    }
+}
+
+const sol5 = document.querySelector('.sol5')
+
+function playsol5() {
+    if (sol5.paused) {
+        sol5.play();
+    } else {
+        sol5.pause();
+    }
+}
+
+const la3 = document.querySelector('.la3')
+
+function playla3() {
+    if (la3.paused) {
+        la3.play();
+    } else {
+        la3.pause();
+    }
+}
+
+const la4 = document.querySelector('.la4')
+
+function playla4() {
+    if (la4.paused) {
+        la4.play();
+    } else {
+        la4.pause();
+    }
+}
+
+const la5 = document.querySelector('.la5')
+
+function playla5() {
+    if (la5.paused) {
+        la5.play();
+    } else {
+        la5.pause();
+    }
+}
+
+const si3 = document.querySelector('.si3')
+
+function playsi3() {
+    if (si3.paused) {
+        si3.play();
+    } else {
+        si3.pause();
+    }
+}
+
+const si4 = document.querySelector('.si4')
+
+function playsi4() {
+    if (si4.paused) {
+        si4.play();
+    } else {
+        si4.pause();
+    }
+}
+
+const si5 = document.querySelector('.si5')
+
+function playsi5() {
+    if (si5.paused) {
+        si5.play();
+    } else {
+        si5.pause();
+    }
+}
+
+btn.addEventListener('click', () => {
+    playAudioBtn()
+})
+
+function playAudioBtn() {
+    if (audio.paused) {
+        audio.play();
+        btn.innerHTML = "off";
+    } else {
+        audio.pause();
+        btn.innerHTML = "on";
+    }
+}
+
+bloc[0].addEventListener('click', () => {
+    playdo3(do3.paused)
+    bloc[0].style.transform = 'scale(.9)'
+})
+bloc[0].addEventListener('mouseout', () => {
+    do3.pause()
+    bloc[0].style.transform = 'scale(1)'
+})
+bloc[1].addEventListener('click', () => {
+    playre3(re3.paused)
+    bloc[1].style.transform = 'scale(.9)'
+})
+bloc[1].addEventListener('mouseout', () => {
+    re3.pause()
+    bloc[1].style.transform = 'scale(1)'
+})
+bloc[2].addEventListener('click', () => {
+    playmi3(mi3.paused)
+    bloc[2].style.transform = 'scale(.9)'
+})
+bloc[2].addEventListener('mouseout', () => {
+    mi3.pause()
+    bloc[2].style.transform = 'scale(1)'
+})
+bloc[3].addEventListener('click', () => {
+    playfa3(fa3.paused)
+    bloc[3].style.transform = 'scale(.9)'
+})
+bloc[3].addEventListener('mouseout', () => {
+    fa3.pause()
+    bloc[3].style.transform = 'scale(1)'
+})
+bloc[4].addEventListener('click', () => {
+    playsol3(sol3.paused)
+    bloc[4].style.transform = 'scale(.9)'
+})
+bloc[4].addEventListener('mouseout', () => {
+    sol3.pause()
+    bloc[4].style.transform = 'scale(1)'
+})
+bloc[5].addEventListener('click', () => {
+    playla3(la3.paused)
+    bloc[5].style.transform = 'scale(.9)'
+})
+bloc[5].addEventListener('mouseout', () => {
+    la3.pause()
+    bloc[5].style.transform = 'scale(1)'
+})
+bloc[6].addEventListener('click', () => {
+    playsi3(si3.paused)
+    bloc[6].style.transform = 'scale(.9)'
+})
+bloc[6].addEventListener('mouseout', () => {
+    si3.pause()
+    bloc[6].style.transform = 'scale(1)'
+})
+bloc[7].addEventListener('click', () => {
+    playdo4(do4.paused)
+    bloc[7].style.transform = 'scale(.9)'
+})
+bloc[7].addEventListener('mouseout', () => {
+    do4.pause()
+    bloc[7].style.transform = 'scale(1)'
+})
+bloc[8].addEventListener('click', () => {
+    playre4(re4.paused)
+    bloc[8].style.transform = 'scale(.9)'
+})
+bloc[8].addEventListener('mouseout', () => {
+    re4.pause()
+    bloc[8].style.transform = 'scale(1)'
+})
+bloc[9].addEventListener('click', () => {
+    playmi4(mi4.paused)
+    bloc[9].style.transform = 'scale(.9)'
+})
+bloc[9].addEventListener('mouseout', () => {
+    mi4.pause()
+    bloc[9].style.transform = 'scale(1)'
+})
+bloc[10].addEventListener('click', () => {
+    playfa4(fa4.paused)
+    bloc[10].style.transform = 'scale(.9)'
+})
+bloc[10].addEventListener('mouseout', () => {
+    fa4.pause()
+    bloc[10].style.transform = 'scale(1)'
+})
+bloc[11].addEventListener('click', () => {
+    playsol4(sol4.paused)
+    bloc[11].style.transform = 'scale(.9)'
+})
+bloc[11].addEventListener('mouseout', () => {
+    sol4.pause()
+    bloc[11].style.transform = 'scale(1)'
+})
+bloc[12].addEventListener('click', () => {
+    playla4(la4.paused)
+    bloc[12].style.transform = 'scale(.9)'
+})
+bloc[12].addEventListener('mouseout', () => {
+    la4.pause()
+    bloc[12].style.transform = 'scale(1)'
+})
+bloc[13].addEventListener('click', () => {
+    playsi4(si4.paused)
+    bloc[13].style.transform = 'scale(.9)'
+})
+bloc[13].addEventListener('mouseout', () => {
+    si4.pause()
+    bloc[13].style.transform = 'scale(1)'
+})
+bloc[14].addEventListener('click', () => {
+    playdo5(do5.paused)
+    bloc[14].style.transform = 'scale(.9)'
+})
+bloc[14].addEventListener('mouseout', () => {
+    do5.pause()
+    bloc[14].style.transform = 'scale(1)'
+})
+bloc[15].addEventListener('click', () => {
+    playre5(re5.paused)
+    bloc[15].style.transform = 'scale(.9)'
+
+})
+bloc[15].addEventListener('mouseout', () => {
+    re5.pause()
+    bloc[15].style.transform = 'scale(1)'
+})
+bloc[16].addEventListener('click', () => {
+    playmi5(mi5.paused)
+    bloc[16].style.transform = 'scale(.9)'
+})
+bloc[16].addEventListener('mouseout', () => {
+    mi5.pause()
+    bloc[16].style.transform = 'scale(1)'
+})
+bloc[17].addEventListener('click', () => {
+    playfa5(fa5.paused)
+    bloc[17].style.transform = 'scale(.9)'
+})
+bloc[17].addEventListener('mouseout', () => {
+    fa5.pause()
+    bloc[17].style.transform = 'scale(1)'
+})
+bloc[18].addEventListener('click', () => {
+    playsol5(sol5.paused)
+    bloc[18].style.transform = 'scale(.9)'
+})
+bloc[18].addEventListener('mouseout', () => {
+    // playAudiod(audioo.play())
+    sol5.pause()
+    bloc[18].style.transform = 'scale(1)'
+})
+bloc[19].addEventListener('click', () => {
+    playla5(la5.paused)
+    bloc[19].style.transform = 'scale(.9)'
+})
+bloc[19].addEventListener('mouseout', () => {
+    la5.pause()
+    bloc[19].style.transform = 'scale(1)'
+})
+bloc[20].addEventListener('click', () => {
+    playsi5(si5.paused)
+    bloc[20].style.transform = 'scale(.9)'
+})
+bloc[20].addEventListener('mouseout', () => {
+    si5.pause()
+    bloc[20].style.transform = 'scale(1)'
+})
+//
+bloc[21].addEventListener('click', () => {
+    bloc[21].style.transform = 'scale(.9)'
+})
+bloc[21].addEventListener('mouseout', () => {
+    bloc[21].style.transform = 'scale(1)'
+})
+bloc[22].addEventListener('click', () => {
+    bloc[22].style.transform = 'scale(.9)'
+})
+bloc[22].addEventListener('mouseout', () => {
+    bloc[22].style.transform = 'scale(1)'
+})
+bloc[23].addEventListener('click', () => {
+
+    bloc[23].style.transform = 'scale(.9)'
+
+})
+bloc[23].addEventListener('mouseout', () => {
+
+    bloc[23].style.transform = 'scale(1)'
+})
+bloc[24].addEventListener('click', () => {
+
+    bloc[24].style.transform = 'scale(.9)'
+
+})
+bloc[24].addEventListener('mouseout', () => {
+    bloc[24].style.transform = 'scale(1)'
+})
+bloc[25].addEventListener('click', () => {
+
+    bloc[25].style.transform = 'scale(.9)'
+})
+bloc[25].addEventListener('mouseout', () => {
+
+    bloc[25].style.transform = 'scale(1)'
+})
+bloc[26].addEventListener('click', () => {
+
+    bloc[26].style.transform = 'scale(.9)'
+})
+bloc[26].addEventListener('mouseout', () => {
+
+    bloc[26].style.transform = 'scale(1)'
+})
+bloc[27].addEventListener('click', () => {
+    bloc[27].style.transform = 'scale(.9)'
+})
+bloc[27].addEventListener('mouseout', () => {
+
+    bloc[27].style.transform = 'scale(1)'
+})
+bloc[28].addEventListener('click', () => {
+
+    bloc[28].style.transform = 'scale(.9)'
+})
+bloc[28].addEventListener('mouseout', () => {
+
+    bloc[28].style.transform = 'scale(1)'
+})
+bloc[29].addEventListener('click', () => {
+
+    bloc[29].style.transform = 'scale(.9)'
+})
+bloc[29].addEventListener('mouseout', () => {
+
+    bloc[29].style.transform = 'scale(1)'
+})
+bloc[30].addEventListener('click', () => {
+
+    bloc[30].style.transform = 'scale(.9)'
+})
+bloc[30].addEventListener('mouseout', () => {
+    bloc[30].style.transform = 'scale(1)'
+})
+
+
+
+
+
+
+
